bf71d58fb4c08aff366b6897631c73a150a9b457
Odongo006 <www.henryodongo006@gmail.com>
2023-01-02 16:47:47 +0300

    Add mobile menu

--- menu.js
+++ menu.js
@@ -1,6 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelectorAll('.nav-links');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-function toggleHamburger() {
-  
-}
+
+
+const openMobilemenu = function () {
+    mobileMenu.classList.remove('hidden');
+    mobileMenu.style.display = 'flex';
+  };
+
+  const closeMobileMenu = function () {
+    mobileMenu.style.display = 'none';
+    mobileMenu.classList.add('hidden');
+  };
