95e891e746b570631a64158df5b341a41d882bc0
mertkantaroglu <mertkantaroglu@gmail.com>
2023-01-03 12:19:35 +0300

    changes in js and css files

--- menu.js
+++ menu.js
@@ -1,21 +1,19 @@
 const hamburger = document.querySelector('.hamburger');
 const menuLinks = document.querySelector('.menu-links');
 const mobileMenu = document.querySelector('.mobile-menu');
-const closeIcon = document.querySelector('.closeIcon');
+const closeIcon = document.querySelector('.cancel-icon');
 
+const openMobileMenu = function () {
+  mobileMenu.classList.remove('hidden');
+  mobileMenu.style.display = 'flex';
+};
 
+hamburger.addEventListener('click', openMobileMenu);
 
-const openMobilemenu = function () {
-    mobileMenu.classList.remove('hidden');
-    mobileMenu.style.display = 'flex';
-  };
+const closeMobileMenu = function () {
+  mobileMenu.style.display = 'none';
+  mobileMenu.classList.add('hidden');
+};
 
-  hamburger.addEventListener('click',openMobilemenu);
-
-  const closeMobileMenu = function () {
-    mobileMenu.style.display = 'none';
-    mobileMenu.classList.add('hidden');
-  };
-
-  menuLinks.addEventListener('click', closeMobileMenu);
-  closeIcon.addEventListener('click', closeMobileMenu);
+menuLinks.addEventListener('click', closeMobileMenu);
+closeIcon.addEventListener('click', closeMobileMenu);
