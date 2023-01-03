a85efe49abe4b43723b33d0e9afc9cd5c727126f
mertkantaroglu <mertkantaroglu@gmail.com>
2023-01-03 10:06:46 +0300

    changes in js and style files

--- menu.js
+++ menu.js
@@ -1,15 +1,49 @@
 const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelectorAll('.nav-links');
+const menuLinks = document.querySelectorAll('.mobile-menu li');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cancelIcon = document.querySelector('.cancel-icon');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+  cancelIcon.classList.toggle('active');
+});
+
+menuLinks.forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  mobileMenu.classList.remove('active');
+  cancelIcon.classList.remove('active');
+}));
+
+// const hamburger = document.querySelector('.hamburger');
+// const ulCont = document.querySelector('.ul-cont');
+
+// function myFunction() {
+//   hamburger.classList.add('hamburger-disappear');
+//   ulCont.classList.add('ul-cont-appear');
+// }
+
+// ulCont.addEventListener('click', ((e) => {
+//   if (e.target.classList[0] === 'closeIcon') {
+//     hamburger.classList.remove('hamburger-disappear');
+//     ulCont.classList.remove('ul-cont-appear');
+//   } else if (e.target.classList[0] === 'nav-link') {
+//     hamburger.classList.remove('hamburger-disappear');
+//     ulCont.classList.remove('ul-cont-appear');
+//   }
+// }));
 
 
+// const menu = document.querySelector('.menu-icon');
+// const closeItems = document.querySelectorAll('.closing');
+// const displayMobileMenu = () => {
+//   menu.style.display = 'block';
+// };
+// const closeMobileMenu = () => {
+//   menu.style.display = 'none';
+// };
+// document
+//   .querySelector('.openMenu')
+//   .addEventListener('click', displayMobileMenu);
+// closeItems.forEach((element) => element.addEventListener('click', closeMobileMenu));
 
-const openMobilemenu = function () {
-    mobileMenu.classList.remove('hidden');
-    mobileMenu.style.display = 'flex';
-  };
-
-  const closeMobileMenu = function () {
-    mobileMenu.style.display = 'none';
-    mobileMenu.classList.add('hidden');
-  };
