a904edbad3d44a00d898aa4e46a687383621ec4f
mertkantaroglu <mertkantaroglu@gmail.com>
2023-01-06 11:17:00 +0300

    update validation

--- JS/validation.js
+++ JS/validation.js
@@ -1,51 +1,32 @@
-// const form = document.querySelector('.contact-form');
-// const email = document.getElementById('email');
-// // const emailError = document.querySelector('#email + .email-error');
-// const emailError = document.querySelector('.email-error');
+const form = document.querySelector('.contact-form');
+const email = document.getElementById('email');
+const emailError = document.querySelector('#email + .error');
 
-// // const emailRegx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+// const emailRegx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
-// email.addEventListener('input', (event) => {
-//   if (email.validity.valid) {
-//     emailError.textContent = '';
-//     emailError.className = 'email-error';
-//   } else {
-//     showError();
-//   }
-// });
-
-// function showError() {
-//   if (email.validity.valueMissing) {
-//     emailError.textContent = 'Email can\'t be empty';
-//   } else if (email.validity.typeMismatch) {
-//     emailError.textContent = 'You need to enter a valid email address';
-//   } else if (email !== email.lowerCase()) {
-//     email.textContent = 'Email can only be in lower cases';
-//   }
-
-//   emailError.className = 'email-error active';
-// }
-
-// form.addEventListener('submit', (event) => {
-//   if (!email.validity.valid) {
-//     showError();
-//     event.preventDefault();
-//   }
-// });
-
-
-const contactForm = document.querySelector('#form');
-const errorMessage = document.querySelector('#error-message');
-
-
-contactForm.addEventListener('submit', (event) => {
-  const email = contactForm.elements.email.value;
-
-  if (email !== email.toLowerCase()) {
-    event.preventDefault();
-    errorMessage.innerHTML = 'Email field only can include lowercase characters.';
-    errorMessage.classList.remove('hidden');
+email.addEventListener('input', (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = '';
+    emailError.className = 'error';
   } else {
-    errorMessage.classList.add('hidden');
+    showError();
   }
 });
+
+form.addEventListener('submit', (event) => {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = 'Email can\'t be empty';
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = 'You need to enter a valid email address';
+  } else if (email.validity.patternMismatch || email !== email.lowerCase()) {
+    emailError.textContent = 'Please type for email with lowercase letters';
+  }
+  emailError.className = 'email-error active';
+}
