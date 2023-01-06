820d79c48d8113a4202de7448b5ea41adc6cc6d5
mertkantaroglu <mertkantaroglu@gmail.com>
2023-01-06 17:16:44 +0300

    fix styling problems

--- menu.js
+++ menu.js
@@ -28,8 +28,7 @@ const storeData = [
   {
     name: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
-    imagesrc: 'Images/Snapshoot_Portfolio_project5',
-    imagesrc2: 'Images/Snapshoot_Portfolio_project5',
+    imagesrc: 'Images/Snapshoot Portfolio (1).png',
     technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
     link: 'link_to_live_version.com',
     content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
@@ -70,14 +69,14 @@ const popupsProjectImage = document.querySelector('.popup-img');
 const popupsProjectTitle = document.querySelector('.popups-proj-title');
 const popupsProjectDesktop = document.querySelector('.popups-project-p-desktop');
 const popupsProjectMobile = document.querySelector('.popups-project-p-mobile');
-const tagsUp = document.querySelectorAll('.card-tagsLi');
-const tagsDown = document.querySelectorAll('.card-tagsLi1');
+const tagsUp = document.querySelectorAll('.card-tags-up');
+const tagsDown = document.querySelectorAll('.card-tags-down');
 
 const openModal = function () {
   overlay.classList.remove('hidden');
   modal.style.display = 'flex';
-  // eslint-disable-next-line no-use-before-define
-  popupsProjectImage.src = storeData[0].imagesrc2;
+
+  popupsProjectImage.src = storeData[0].imagesrc;
   popupsProjectTitle.innerHTML = storeData[0].name;
   popupsProjectDesktop.innerHTML = storeData[0].content1;
   popupsProjectMobile.innerHTML = storeData[0].content2;
@@ -94,37 +93,3 @@ const closeModal = function () {
 };
 
 closeModalBtn.addEventListener('click', closeModal);
-
-// Form validation
-const form = document.querySelector('.form-group');
-const email = document.getElementById('email');
-
-// Invalid error message
-const setError = (message) => {
-  const errorDisplay = document.querySelector('.error');
-  errorDisplay.innerText = message;
-};
-
-// valid success message
-const setSuccess = () => {
-  const errorDisplay = document.querySelector('.error');
-  errorDisplay.innerText = '';
-};
-
-const inputValidation = (e) => {
-  const emailValue = email.value.trim();
-
-  if (emailValue === '') {
-    setError('Email is required');
-    e.preventDefault();
-  } else if (emailValue.toLowerCase() !== emailValue) {
-    setError('Provide a valid email address');
-    e.preventDefault();
-  } else {
-    setSuccess();
-  }
-};
-
-form.addEventListener('submit', (e) => {
-  inputValidation(e);
-});
