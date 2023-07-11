4ceec8bf4bf3d62781733a7ac41ec9aa0dcf3343
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-07-11 21:00:14 +0300

    Update menu file

--- menu.js
+++ menu.js
@@ -31,43 +31,43 @@ const closeModalBtn = document.querySelector('.closeIcon-popup');
 // Card Objects //
 const projects = [
   {
-    name: 'Tonic',
-    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
-    image: 'Images/Snapshoot_Portfolio_project5.png',
-    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
-    link: 'link_to_live_version.com',
+  //   name: 'Tonic',
+  //   description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+  //   image: 'Images/Snapshoot_Portfolio_project5.png',
+  //   technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+  //   link: 'link_to_live_version.com',
 
-    content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the...',
-    content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
-    tags1: ['HTML', 'css', 'Javascript'],
-    tags2: ['Github', 'Ruby', 'Frontend'],
-  },
-  {
-    name: 'Multi Post Stories',
-    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    image: 'Images/Snapshoot Portfolio (3).png',
-    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
-    link: 'link_to_live_version.com',
-    content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing...',
-    content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
-  },
-  {
-    name: 'Facebook 360',
-    description: 'Exploring the future of media in Facebook,s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-    image: 'Images/Snapshoot Portfolio.png',
-    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
-    link: 'link_to_live_version.com',
-    content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing...',
-    content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
-  },
-  {
-    name: 'Uber Navigation',
-    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    image: 'Images/Snapshoot Portfolio (1).png',
-    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
-    link: 'link_to_live_version.com',
-    content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing...',
-    content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
+  //   content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the...',
+  //   content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
+  //   tags1: ['HTML', 'css', 'Javascript'],
+  //   tags2: ['Github', 'Ruby', 'Frontend'],
+  // },
+  // {
+  //   name: 'Multi Post Stories',
+  //   description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+  //   image: 'Images/Snapshoot Portfolio (3).png',
+  //   technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+  //   link: 'link_to_live_version.com',
+  //   content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing...',
+  //   content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
+  // },
+  // {
+  //   name: 'Facebook 360',
+  //   description: 'Exploring the future of media in Facebook,s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+  //   image: 'Images/Snapshoot Portfolio.png',
+  //   technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+  //   link: 'link_to_live_version.com',
+  //   content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing...',
+  //   content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
+  // },
+  // {
+  //   name: 'Uber Navigation',
+  //   description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+  //   image: 'Images/Snapshoot Portfolio (1).png',
+  //   technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+  //   link: 'link_to_live_version.com',
+  //   content1: 'Lorem ipsum it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing...',
+  //   content2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus mollitia saepe maxime vitae nostrum repellat eligendi amet voluptas modi repudiandae necessitatibus animi harum obcaecati at omnis laboriosam sapiente eum rem velit molestias',
   },
 ];
 
@@ -78,70 +78,34 @@ const popupsProjectMobile = document.querySelector('.popups-project-p-mobile');
 const tagsUp = document.querySelectorAll('.card-tags-up');
 const tagsDown = document.querySelectorAll('.card-tags-down');
 
-// Opening 1st Card //
-const openModal1 = function () {
+function openModal(index) {
   overlay.classList.remove('hidden');
   modal.style.display = 'flex';
 
-  // Populating cards with object items //
-  popupsProjectImage.src = projects[0].image;
-  popupsProjectTitle.innerHTML = projects[0].name;
-  popupsProjectDesktop.innerHTML = projects[0].content1;
-  popupsProjectMobile.innerHTML = projects[0].content2;
-  tagsUp.innerHTML = projects[0].tags1;
-  tagsDown.innerHTML = projects[0].tags2;
+  popupsProjectImage.src = projects[index].image;
+  popupsProjectTitle.innerHTML = projects[index].name;
+  popupsProjectDesktop.innerHTML = projects[index].content1;
+  popupsProjectMobile.innerHTML = projects[index].content2;
+  tagsUp.innerHTML = projects[index].tags1;
+  tagsDown.innerHTML = projects[index].tags2;
   document.documentElement.scrollTop = 0;
-};
+}
 
-openModalBtn1.addEventListener('click', openModal1);
+openModalBtn1.addEventListener('click', function() {
+  openModal(0);
+});
 
-// Opening 2nd Card //
-const openModal2 = function () {
-  overlay.classList.remove('hidden');
-  modal.style.display = 'flex';
+openModalBtn2.addEventListener('click', function() {
+  openModal(1);
+});
 
-  popupsProjectImage.src = projects[1].image;
-  popupsProjectTitle.innerHTML = projects[1].name;
-  popupsProjectDesktop.innerHTML = projects[1].content1;
-  popupsProjectMobile.innerHTML = projects[1].content2;
-  tagsUp.innerHTML = projects[1].tags1;
-  tagsDown.innerHTML = projects[1].tags2;
-  document.documentElement.scrollTop = 0;
-};
+openModalBtn3.addEventListener('click', function() {
+  openModal(2);
+});
 
-openModalBtn2.addEventListener('click', openModal2);
-
-// Opening 3rd Card //
-const openModal3 = function () {
-  overlay.classList.remove('hidden');
-  modal.style.display = 'flex';
-
-  popupsProjectImage.src = projects[2].image;
-  popupsProjectTitle.innerHTML = projects[2].name;
-  popupsProjectDesktop.innerHTML = projects[2].content1;
-  popupsProjectMobile.innerHTML = projects[2].content2;
-  tagsUp.innerHTML = projects[2].tags1;
-  tagsDown.innerHTML = projects[2].tags2;
-  document.documentElement.scrollTop = 0;
-};
-
-openModalBtn3.addEventListener('click', openModal3);
-
-// Opening 4th Card //
-const openModal4 = function () {
-  overlay.classList.remove('hidden');
-  modal.style.display = 'flex';
-
-  popupsProjectImage.src = projects[3].image;
-  popupsProjectTitle.innerHTML = projects[3].name;
-  popupsProjectDesktop.innerHTML = projects[3].content1;
-  popupsProjectMobile.innerHTML = projects[3].content2;
-  tagsUp.innerHTML = projects[3].tags1;
-  tagsDown.innerHTML = projects[3].tags2;
-  document.documentElement.scrollTop = 0;
-};
-
-openModalBtn4.addEventListener('click', openModal4);
+openModalBtn4.addEventListener('click', function() {
+  openModal(3);
+});
 
 // Closing Popup //
 const closeModal = function () {
@@ -159,16 +123,3 @@ document.getElementById('contact-form').onsubmit = (e) => {
     document.getElementById('error').innerHTML = 'Email must be in lowercase';
   }
 };
-
-
-// Debugger Exercise //
-
-const works = document.getElementsById - incorrect / querySelector with '.' to choose a class ('works');
-
-works.onsubmit = (event) => {
-  const email = document.getElementById('email').value;
-  if (email !== email.toLowerCase()) {
-    e - inccorect- / should match 'event' .preventDefault();
-    document.getElementById('error').appendChild - incorrect / should be innerHTML to write down = 'Email must be in lowercase';
-  }
-}, - incorrect / should be semicolumn
